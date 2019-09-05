2b224155e297f7510e9bf494fe945a04ac1fc089
sali0 <saadali93@gmail.com>
2019-09-05 00:16:33 -0500

    Trying to consolidate commands

--- src/commands/VideoData.js
+++ src/commands/VideoData.js
@@ -1,24 +1,45 @@
 const {Command, flags} = require('@oclif/command')
-const fetcher = require('../lib/FetchVideoData')
-const transcriptor = require('../lib/FetchTranscript')
+const Fetcher = require('../lib/FetchVideoData')
+const Transcriptor = require('../lib/FetchTranscript')
+const ChannelUtils = require("../lib/ChannelUtils")
 
 class VideoData extends Command {
   async run() {
     const {flags} = this.parse(VideoData)
-    const videoId = flags.video
-    let data = await fetcher(videoId)
-    data.transcript = await transcriptor(videoId)
 
-    if (flags.export) {
+    if (flags.video) {
+      const videoId = flags.video;
+      let data = await Fetcher(videoId);
+      data.transcript = await Transcriptor(videoId);
+
+      if (flags.export) {
+      }
+      this.log(data);
+    } 
+
+    if (flags.channel) {
+      const channelId = flags.channel
+      const playlistId = await ChannelUtils.GetPlaylistId(channelId)
+      const videoIdArray = await ChannelUtils.GetChannelVideoIds(playlistId)
+
+      let completeChannelData = []
+      videoIdArray.forEach((item) => {
+        let videoData = await Fetcher(item)
+        videoData.transcript = await Transcriptor(item)
+
+        completeChannelData.push(videoData)
+      })
+
+      this.log(completeChannelData)
     }
-    this.log(data)
   }
 }
 
-VideoData.description = `Gets Youtube video data for a single video. Outputs a json file`
+VideoData.description = `Gets Youtube video data for a single video or a whole channel's videos. Outputs a json file`
 
 VideoData.flags = {
-  video: flags.string({char: 'v', description: 'List video ID'}),
+  video: flags.string({char: 'v', description: 'Video ID'}),
+  channel: flags.string({char: 'c', description: 'Channel ID'}),
   export: flags.string({char: 'e', description: 'Export to JSON'})
 }
 
